aabe212eba2d7027a7a4ae6f3e3fe3fde11a6fcc
Chloe <cgreenstreet01@gmail.com>
2024-09-15 21:11:26 +0100

    Structure clean and change

---
+++ src/acceptance/java/Stepdefs.java
@@ -0,0 +1,115 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Stepdefs {
+    @Given("a vertical grid with {int} columns and {int} rows is created")
+    public void a_vertical_grid_with_columns_and_rows_is_created(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("a new game is created")
+    public void a_new_game_is_created() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("player {int} selects pink pieces")
+    public void player_selects_pink_pieces(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("player {int} selects white pieces")
+    public void player_selects_white_pieces(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("player {int} makes a move in column {int}")
+    public void a_player_makes_a_move_in_column(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("column {int} has {int} pieces in it")
+    public void column_has_pieces_in_it(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("player {int} has a piece at position {int} x {int}")
+    public void player_has_a_piece_at_position_x(Integer int1, Integer int2, Integer int3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the game has a result")
+    public void the_game_has_a_result() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    //When
+    @When("any player makes a move in column {int}")
+    public void any_player_makes_a_move_in_column(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("there is a piece at every position")
+    public void there_is_a_piece_at_every_position() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    //Then
+    @Then("a pink piece should be on the grid at position {int} x {int}")
+    public void a_pink_piece_should_be_on_the_grid_at_position_x(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("a white piece should be on the grid at position {int} x {int}")
+    public void a_white_piece_should_be_on_the_grid_at_position_x(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the player is prompted to select a different column")
+    public void the_player_is_prompted_to_select_a_different_column() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("player {int} has won the game")
+    public void player_has_won_the_game(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the players have drawn the game")
+    public void the_players_have_drawn_the_game() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("player {int} has lost the game")
+    public void player_has_lost_the_game(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the game is adjourned")
+    public void the_game_is_adjourned() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("a new empty board is created")
+    public void a_new_empty_board_is_created() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
