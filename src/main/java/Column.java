5325f56205e20fd40ca14c7fef4fb8331d823df9
Chloe <cgreenstreet01@gmail.com>
2024-09-24 13:26:16 +0100

    More validation added and finished column tests

--- src/main/java/Column.java
+++ src/main/java/Column.java
@@ -1,13 +1,15 @@
-import static java.util.Objects.requireNonNull;
-
 public class Column {
+
     private final int value;
 
     private Column(final int value) {
-        this.value = value;
+        this.value = value; //Requires no validation due to being a primitive
     }
 
     public static Column create(final int value) {
+        if(value <= 0){
+            throw new IndexOutOfBoundsException("column value must be greater than zero");
+        }
         return new Column(value);
     }
 
--- src/test/ColumnTest.java
+++ src/test/ColumnTest.java
@@ -1,15 +1,23 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIndexOutOfBoundsException;
 
 class ColumnTest {
-    @ParameterizedTest
-    @CsvSource(value = {" ", ",","","/","/n"})
-    void columnValueIsNotBlank(final int columnValue) {
-        assertThatIllegalStateException()
-                .isThrownBy(() -> Column.create(columnValue))
-                .withMessage("columnValue can not be blank");
+
+    @Test
+    void canGetValueFromColumn() {
+        assertThat(Column.create(1).getValue()).isEqualTo(1);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1,-10})
+    void canGetValueFromColumn2(final int value) {
+        assertThatIndexOutOfBoundsException()
+                .isThrownBy(()-> Column.create(value).getValue())
+                .withMessage("column value must be greater than zero");
+    }
+
 }
